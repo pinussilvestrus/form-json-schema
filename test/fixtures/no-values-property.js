370fee2dcd1e5b98ea8255ba9ae95c0be4d94499
Niklas Kiefer <niklas.kiefer@camunda.com>
2023-10-06 09:25:57 +0200

    fix: make button action optional
    https://github.com/bpmn-io/form-js/blob/develop/packages/form-js-viewer/src/render/components/form-fields/Button.js#L11

--- test/fixtures/no-values-property.js
+++ test/fixtures/no-values-property.js
@@ -11,35 +11,35 @@ export const form = {
 export const errors = [
   {
     instancePath: '/components/0',
-    schemaPath: '#/properties/components/items/allOf/0/allOf/3/then/anyOf/0/required',
+    schemaPath: '#/properties/components/items/allOf/0/allOf/2/then/anyOf/0/required',
     keyword: 'required',
     params: { missingProperty: 'values' },
     message: "must have required property 'values'"
   },
   {
     instancePath: '/components/0',
-    schemaPath: '#/properties/components/items/allOf/0/allOf/3/then/anyOf/1/required',
+    schemaPath: '#/properties/components/items/allOf/0/allOf/2/then/anyOf/1/required',
     keyword: 'required',
     params: { missingProperty: 'valuesKey' },
     message: "must have required property 'valuesKey'"
   },
   {
     instancePath: '/components/0',
-    schemaPath: '#/properties/components/items/allOf/0/allOf/3/then/anyOf/2/required',
+    schemaPath: '#/properties/components/items/allOf/0/allOf/2/then/anyOf/2/required',
     keyword: 'required',
     params: { missingProperty: 'valuesExpression' },
     message: "must have required property 'valuesExpression'"
   },
   {
     instancePath: '/components/0',
-    schemaPath: '#/properties/components/items/allOf/0/allOf/3/then/anyOf',
+    schemaPath: '#/properties/components/items/allOf/0/allOf/2/then/anyOf',
     keyword: 'anyOf',
     params: {},
     message: 'must match a schema in anyOf'
   },
   {
     instancePath: '/components/0',
-    schemaPath: '#/properties/components/items/allOf/0/allOf/3/if',
+    schemaPath: '#/properties/components/items/allOf/0/allOf/2/if',
     keyword: 'if',
     params: { failingKeyword: 'then' },
     message: 'must match "then" schema'
