1eb055c390315aa0bf85e61b90d220fef3b0b64c
Niklas Kiefer <niklas.kiefer@camunda.com>
2023-10-06 09:42:21 +0200

    fix: make date subType optional

--- test/fixtures/no-subtype.js
+++ test/fixtures/no-subtype.js
@@ -8,19 +8,4 @@ export const form = {
   ]
 };
 
-export const errors = [
-  {
-    instancePath: '/components/0',
-    schemaPath: '#/properties/components/items/allOf/0/allOf/1/then/required',
-    keyword: 'required',
-    params: { missingProperty: 'subtype' },
-    message: "must have required property 'subtype'"
-  },
-  {
-    instancePath: '/components/0',
-    schemaPath: '#/properties/components/items/allOf/0/allOf/1/if',
-    keyword: 'if',
-    params: { failingKeyword: 'then' },
-    message: 'must match "then" schema'
-  }
-];
+export const errors = null;
